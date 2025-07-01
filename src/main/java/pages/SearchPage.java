b3145a9bc4c1cd48e391de8bbe8200faac56fe97
abhishek4041 <sharma.abhishek4041@gmail.com>
2025-07-01 14:26:30 +0530

    added positive tests

--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class SearchPage {
 
@@ -31,7 +36,61 @@ public class SearchPage {
     }
 
     public void searchProduct(String productName) {
-        getSearchBox().sendKeys(productName);
-        getSearchBox().submit();
+        WebElement searchBox = getSearchBox();
+        searchBox.clear();
+        searchBox.sendKeys(productName);
+        searchBox.submit(); // Correct submission
     }
+
+    public List<WebElement> getAllProductsUsingCSS(){
+
+        return driver.findElements(By.cssSelector("li.product-item a.product-item-link"));
+    }
+
+    public WebElement pageTitle() {
+        WebDriverWait waitForElementToBeVisible = new WebDriverWait(driver, Duration.ofSeconds(8));
+        return waitForElementToBeVisible.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"maincontent\"]/div[1]/h1/span")));
+    }
+
+    public void addProductToTheCartAndProceedToCheckout() {
+        // Click on the first product
+        WebElement selectProduct = driver.findElement(By.xpath("//*[@id='maincontent']/div[3]/div[1]/div[2]/div[2]/ol/li[1]/div/div/strong/a"));
+        selectProduct.click();
+
+        // Optional: Fetch the product title (if you want to verify later)
+        String actualTitle = driver.findElement(By.xpath("//*[@itemprop='name']")).getText();
+        System.out.println("Selected Product: " + actualTitle);
+
+        // Select color
+        WebElement selectColor = driver.findElement(By.xpath("//*[@id='option-label-color-93-item-53']"));
+        selectColor.click();
+
+        // Select size
+        WebElement selectSize = driver.findElement(By.xpath("//*[@id='option-label-size-143-item-168']"));
+        selectSize.click();
+
+        // Click on 'Add to Cart' button
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement addToCartButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@title='Add to Cart']")));
+        addToCartButton.click();
+
+        // Open cart by clicking cart quantity icon
+        WebElement cartQtyButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='counter qty']")));
+        cartQtyButton.click();
+
+        // Click 'Proceed to Checkout' button
+        WebElement proceedToCheckoutButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='top-cart-btn-checkout']")));
+        proceedToCheckoutButton.click();
+    }
+
+    public String validateTheUrlOftheCart(){
+
+        //validate the url of the cart
+
+        String actualUrl = driver.getCurrentUrl();
+
+        return actualUrl;
+    }
+
+
 }
--- src/test/java/tests/search/SearchPagePositiveTests/SearchPagePositiveTests.java
+++ src/test/java/tests/search/SearchPagePositiveTests/SearchPagePositiveTests.java
@@ -2,10 +2,16 @@ package tests.search.SearchPagePositiveTests;
 
 import base.BaseTest;
 import io.qameta.allure.Description;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.SearchPage;
+import utilities.JavascriptUtil;
+
+import java.time.Duration;
 
 public class SearchPagePositiveTests extends BaseTest {
 
@@ -22,4 +28,78 @@ public class SearchPagePositiveTests extends BaseTest {
         boolean isProductDisplayed = searchPage.getFirstProductUsingJS().isDisplayed();
         Assert.assertTrue(isProductDisplayed,"First product is not displayed.");
     }
+
+    @Description("Search a product and validate multiple products are displayed using CSS selector")
+    @Test
+    public void validateMultipleProductsDisplayed(){
+
+        HomePage homePage = new HomePage(driver);
+        SearchPage searchPage = new SearchPage(driver);
+
+
+        searchPage.searchProduct("Tank");
+        int productCount = searchPage.getAllProductsUsingCSS().size();
+
+        Assert.assertTrue(productCount>1, "Multiple products are not being displayed.");
+    }
+
+    @Description("Search a product and click on the first product using JavaScript Executor")
+    @Test
+    public void clickProductUsingJavaScriptExecutor() {
+
+        HomePage homePage = new HomePage(driver);
+        SearchPage searchPage = new SearchPage(driver);
+        JavascriptUtil jsUtil = new JavascriptUtil(driver);
+
+        searchPage.searchProduct("Tank");
+
+        WebElement firstProductLink = searchPage.getAllProductsUsingCSS().get(0);
+
+        jsUtil.clickElementByJS(firstProductLink);
+
+        // Validate that the Product Page is opened
+    }
+
+    @Description("After clicking on the first product, the product detail page should open, " +
+            "and the product title on the detail page should match the name of the product selected.")
+    @Test
+    public void validateSearchResultsCountIsGreaterThanZero() {
+        HomePage homePage = new HomePage(driver);
+        SearchPage searchPage = new SearchPage(driver);
+
+        // Correct: Use HomePage's searchProduct because it correctly submits
+        homePage.searchProduct("Tank");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("search"));
+
+        String expectedPageTitle = "Search results for: 'Tank'";
+        Assert.assertEquals(searchPage.pageTitle().getText(), expectedPageTitle, "The title didn't matched!");
+    }
+
+    @Description("Add first product to the cart and verify the cart URL.")
+    @Test
+    public void validateAddingFirstProductToTheCart() {
+        HomePage homePage = new HomePage(driver);
+        SearchPage searchPage = new SearchPage(driver);
+
+        // Step 1: Search for the product
+        homePage.searchProduct("Tank");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.urlContains("search"));
+
+        // Step 2: Add the product to the cart and proceed to checkout
+        searchPage.addProductToTheCartAndProceedToCheckout();
+
+        // Step 3: Validate the cart/checkout URL
+        String actualCartUrl = searchPage.validateTheUrlOftheCart();
+
+        // Example expected URL - Adjust this as per your app
+        String expectedCartUrl = "https://magento.softwaretestingboard.com/checkout/#shipping";
+
+        Assert.assertEquals(actualCartUrl, expectedCartUrl, "Cart URL is not as expected.");
+    }
+
+
 }
